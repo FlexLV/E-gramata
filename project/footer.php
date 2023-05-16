8026dcc0720a2ca73f18a0d2d35f1e532bc0dccd
Ernests Dejus <78017262+FlexLV@users.noreply.github.com>
2023-05-17 02:31:25 +0300

    Add files via upload

---
+++ project/footer.php
@@ -0,0 +1,28 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Links</h3>
+         <a href="about.php">About</a>
+         <a href="login.php">Shop</a>
+         <a href="contact.php">Contact</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+      <div class="box">
+         <h3>Contact info</h3>
+         <p> <i class="fas fa-envelope"></i> ernisd621@gmail.com </p>
+         <p> <i class="fas fa-phone"></i> +371 22325505 </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Bultu iela 4 </p>
+      </div>
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"> <i class="fa-solid fa-envelope"></i> Gmail </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-github"></i> Github </a>
+      </div>
+   </div>
+   <p class="credit"> &copy; Copyright  <?php echo date('Y'); ?> by <span>Ernests Dejus</span> </p>
+</section>
